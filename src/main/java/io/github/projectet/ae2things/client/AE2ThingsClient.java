cb969b8497f1d006f9293cbc847a59b478da21fe
ProjectET <projectet@hotmail.com>
2022-01-02 13:50:23 +1030

    :crab:

--- src/main/java/io/github/projectet/ae2things/client/AE2ThingsClient.java
+++ src/main/java/io/github/projectet/ae2things/client/AE2ThingsClient.java
@@ -17,19 +17,5 @@ public class AE2ThingsClient implements IAEAddonEntrypoint {
     @Override
     public void onAe2Initialized() {
         ScreenRegistry.<AdvancedInscriberMenu, AdvancedInscriberRootPanel>register(AdvancedInscriberMenu.ADVANCED_INSCRIBER_SHT, AdvancedInscriberRootPanel::new);
-
-
-
-        if (actionType != SlotActionType.CLONE || !player.getAbilities().creativeMode || !this.getCursorStack().isEmpty() || slotIndex < 0) break block50;
-        Slot i = this.slots.get(slotIndex);
-        if (!i.hasStack()) break block39;
-        if(!i.getStack().getItem() instanceof DISKDrive) {
-            ItemStack slot = i.getStack().copy();
-            slot.setCount(slot.getMaxCount());
-            this.setCursorStack(slot);
-        }
-        else {
-            //...
-        }
     }
 }
